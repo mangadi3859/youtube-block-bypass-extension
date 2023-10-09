cfc01f5ab0afd648de13d89aeef99dfa59870129
mangadi3859 <wkomangadi44@gmail.com>
2023-10-09 20:52:14 +0800

    Initial Commir

---
+++ dist/content-script.js
@@ -0,0 +1,38 @@
+"use strict";
+const SESSION_NAME = "__yt_state";
+let isBlocked = sessionStorage.getItem(SESSION_NAME) == "1";
+function start() {
+    if (window.location.pathname != "/watch")
+        return;
+    const id = new URL(window.location.href).searchParams.get("v");
+    if (!id)
+        return console.log("[Youtube Block Bypass] Cannot find video id");
+    const iframeHTML = `<iframe 
+    style="width: 100%; height: auto; aspect-ratio: 16/9;"
+    src="https://www.youtube.com/embed/${id}?autoplay=1"
+    title="Youtube Block Bypass"
+    frameborder="0"
+    allow="accelerometer;
+    autoplay;
+    clipboard-write;
+    encrypted-media; gyroscope;
+    picture-in-picture; web-share"
+    allowfullscreen="">
+    </iframe>
+    `;
+    const player = document.querySelector("#player.ytd-watch-flexy");
+    if (isBlocked) {
+        player.innerHTML = iframeHTML;
+        return;
+    }
+    const blocker = document.querySelector("yt-playability-error-supported-renderers#error-screen");
+    if (!blocker)
+        return console.log("[Youtube Block Bypass] Cannot find blocker element");
+    isBlocked = true;
+    sessionStorage.setItem(SESSION_NAME, "1");
+    blocker.parentNode.innerHTML = iframeHTML;
+}
+document.addEventListener("yt-navigate-finish", () => {
+    start();
+    window.addEventListener("hashchange", start);
+});
